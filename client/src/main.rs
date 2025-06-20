77270c912fb24a2368ae88635ec519b2ee2ce485
alpn <alpn@users.noreply.github.com>
2025-06-20 13:31:52 -0400

    client: introduce clap for command-line parsing and refactor code into modules

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,41 +1,31 @@
-use shared::{BINCODE_CONFIG, WireplugAnnounce, WireplugResponse};
-use std::{
-    env::{self},
-    io::{Read, Write},
-    net::{TcpStream, ToSocketAddrs},
-    thread,
-    time::Duration,
-};
+use clap::Parser;
+use std::{thread, time::Duration};
 
-fn send_announcement<T: ToSocketAddrs>(t: T) {
-    //-> std::io::Result<()> {
-    match TcpStream::connect(t) {
-        Ok(mut s) => {
-            let hello = WireplugAnnounce::new(
-                "alicealicealicealicealicealicealicealicealic",
-                "bobbobbobbobbobbobbobbobbobbobbobbobbobbobbo",
-            );
-            let v = bincode::encode_to_vec(&hello, BINCODE_CONFIG).unwrap();
-            s.write_all(&v).unwrap();
-            let mut res = [0u8; 1024];
-            s.read(&mut res).ok().unwrap();
-            let (response, _): (WireplugResponse, usize) =
-                bincode::decode_from_slice(&res[..], BINCODE_CONFIG).unwrap();
-            println!("response: {:?}", response);
-        }
-        Err(e) => eprintln!("{:?}", e),
-    }
+pub mod protocol;
+pub mod wg_interface;
+
+#[derive(Parser)]
+#[command(version, name="Wireplug", about="", long_about = None)]
+struct Cli {
+    interface_name: String,
+    #[arg(short, long)]
+    config_interface: bool,
 }
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
-        loop {
-            println!("sending announcement..");
-            send_announcement(&args[1]);
-            println!("waiting..");
-            thread::sleep(Duration::from_secs(3));
-        }
-    } else {
-        eprintln!("usage");
+
+    let cli = Cli::parse();
+    let ifname = &cli.interface_name;
+    wg_interface::show_config(ifname);
+
+    if cli.config_interface {
+        wg_interface::configure(ifname);
+        wg_interface::show_config(ifname);
+    }
+
+    println!("attempting to monitor interface {}..", ifname);
+    loop {
+        protocol::monitor_interface(ifname);
+        thread::sleep(Duration::from_secs(protocol::MONITORING_INTERVAL));
     }
 }
