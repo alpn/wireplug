153a6630c24b8d9f76264cd50625da621d558dd5
alpn <alpn@users.noreply.github.com>
2025-06-26 17:20:21 -0400

    client: error handling

--- client/src/protocol.rs
+++ client/src/protocol.rs
@@ -12,51 +12,62 @@ const WIREPLUG_ORG: &str = "wireplug.org:4455";
 pub(crate) const MONITORING_INTERVAL: u64 = 30;
 // WireGuard's rekey interval, and some
 const LAST_HANDSHAKE_MAX: u64 = 125;
-const RETRY_INTERVAL_SEC: u64 = 10;
+const _RETRY_INTERVAL_SEC: u64 = 10;
 
 fn send_announcement(
     initiator_pubkey: &Key,
     peer_pubkey: &Key,
     port: u16,
-) -> Option<WireplugResponse> {
-    match TcpStream::connect(WIREPLUG_ORG) {
-        Ok(mut stream) => {
-            let announcement = WireplugAnnounce::new(
-                &initiator_pubkey.to_base64(),
-                &peer_pubkey.to_base64(),
-                port,
-            );
-            let buf = bincode::encode_to_vec(&announcement, BINCODE_CONFIG).unwrap();
-            stream.write_all(&buf).unwrap();
-            let mut res = [0u8; 1024];
-            stream.read(&mut res).ok().unwrap();
-            let (response, _): (WireplugResponse, usize) =
-                bincode::decode_from_slice(&res[..], BINCODE_CONFIG).unwrap();
-            return Some(response);
-        }
-        Err(e) => eprintln!("error: {:?}", e),
-    }
-    None
+) -> Result<WireplugResponse, std::io::Error> {
+    let mut stream = TcpStream::connect(WIREPLUG_ORG)?;
+    let announcement = WireplugAnnounce::new(
+        &initiator_pubkey.to_base64(),
+        &peer_pubkey.to_base64(),
+        port,
+    );
+
+    let buf = bincode::encode_to_vec(&announcement, BINCODE_CONFIG).map_err(|e| {
+        std::io::Error::new(std::io::ErrorKind::Other, format!("encoding error: {e}"))
+    })?;
+
+    stream.write_all(&buf)?;
+    let mut res = [0u8; 1024];
+    stream.read(&mut res)?;
+    let (response, _): (WireplugResponse, usize) =
+        bincode::decode_from_slice(&res[..], BINCODE_CONFIG).map_err(|e| {
+            std::io::Error::new(std::io::ErrorKind::Other, format!("encoding error: {e}"))
+        })?;
+
+    Ok(response)
 }
 
-pub(crate) fn monitor_interface(if_name: &String) {
-    let iface = if_name.parse().unwrap();
-    let device = Device::get(&iface, Backend::default()).unwrap();
-    if device.public_key.is_none() {
-        println!("{} is not configured", if_name);
-        return;
-    }
+pub(crate) fn monitor_interface(if_name: &String) -> Result<(), std::io::Error> {
+    let iface = if_name.parse()?;
+    let device = Device::get(&iface, Backend::default())?;
+    let Some(pubkey) = &device.public_key.clone() else {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("{} is not configured", if_name),
+        ));
+    };
 
-    let pubkey = &device.public_key.clone().unwrap();
-    let port = device.listen_port.unwrap();
+    let Some(port) = device.listen_port else {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("{} is not configured", if_name),
+        ));
+    };
     let now = SystemTime::now();
 
     for peer in device.peers {
         print!("\tpeer: {} .. ", &peer.config.public_key.to_base64());
         let annouce;
         if let Some(last_handshake) = peer.stats.last_handshake_time {
-            let duration = now.duration_since(last_handshake).unwrap();
-            print!("last handshake {} seconds ago => ", duration.as_secs(),);
+            let duration = now
+                .duration_since(last_handshake)
+                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, format!("{e}")))?;
+
+
             annouce = duration > Duration::from_secs(LAST_HANDSHAKE_MAX);
         } else {
             print!("no previous handshakes => ");
@@ -64,23 +75,21 @@ pub(crate) fn monitor_interface(if_name: &String) {
         }
         if annouce {
             print!("sending announcement.. ");
-            match send_announcement(&pubkey, &peer.config.public_key, port) {
-                Some(r) => {
-                    if let Some(ip) = r.ip {
-                        println!("| wireplug.org: peer is @{}", ip);
-                        wg_interface::update(&iface, &peer, ip);
-                    } else {
-                        println!("| wireplug.org: peer is unknown");
-                    }
+            match send_announcement(&pubkey, &peer.config.public_key, port)?.ip {
+                Some(ip) => {
+                    println!("| wireplug.org: peer is @{}", ip);
+                    wg_interface::update(&iface, &peer, ip);
                 }
-                None => (),
+                None => println!("| wireplug.org: peer is unknown"),
             }
         } else {
             println!("doing nothing");
         }
     }
+    Ok(())
 }
 
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -93,3 +102,4 @@ mod tests {
         println!("res= {:?}", res);
     }
 }
+*/
