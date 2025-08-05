8a9109a5271012d1a87ce1419c9b2bedb0de93cb
alpn <alpn@users.noreply.github.com>
2025-08-05 14:31:34 -0400

    shared: add TmpLogger

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,4 +1,6 @@
 use bincode::config::Configuration;
+use colored::Colorize;
+use log::{Level, Log, Metadata, Record};
 
 pub mod protocol;
 
@@ -9,3 +11,26 @@ pub const BINCODE_CONFIG: Configuration<
 > = bincode::config::standard()
     .with_fixed_int_encoding()
     .with_limit::<4096>();
+
+pub struct TmpLogger;
+
+impl Log for TmpLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            let level_str = match record.level() {
+                Level::Error => "[E]".red(),
+                Level::Warn => "[!]".yellow(),
+                Level::Info => "[*]".dimmed(),
+                Level::Debug => "[D]".blue(),
+                Level::Trace => "[T]".purple(),
+            };
+            println!("{level_str} {}", record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
