5be9dd8856fcaf5d24b9024e28122af496ea7d80
alpn <alpn@users.noreply.github.com>
2025-08-06 10:13:43 -0400

    client: extract main logic from main() to start() and monitor_interface()

--- client/src/main.rs
+++ client/src/main.rs
@@ -18,20 +18,7 @@ struct Cli {
     no_nat: bool,
 }
 
-fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
-    let ifname = &cli.interface_name;
-    wg_interface::show_config(ifname)?;
-
-    if let Some(config_file) = cli.config {
-        let config = config::read_from_file(&config_file)?;
-        wg_interface::configure(ifname, &config)?;
-        wg_interface::show_config(ifname)?;
-        /*
-        println!("waiting for peers to attempt handshakes..");
-        std::thread::sleep(Duration::from_secs(shared::COMMON_PKA as u64 + 5));
-         */
-    }
+fn monitor_interface(ifname: &String, traverse_nat: bool) -> anyhow::Result<()> {
     let mut netmon = netstat::NetworkMonitor::new();
     loop {
         let Some(listen_port) = wg_interface::get_port(ifname) else {
@@ -39,7 +26,7 @@ fn main() -> anyhow::Result<()> {
         };
         let inactive_peers = wg_interface::get_inactive_peers(ifname)?;
         if !inactive_peers.is_empty() {
-            let port_to_announce = if netmon.has_changed() && !cli.no_nat {
+            let port_to_announce = if netmon.has_changed() && traverse_nat {
                 let new_listen_port = utils::get_random_port();
                 let nat = nat::detect_kind(new_listen_port)?;
                 println!("NAT: {nat:?}");
@@ -71,5 +58,32 @@ fn main() -> anyhow::Result<()> {
             }
         }
         thread::sleep(Duration::from_secs(shared::protocol::MONITORING_INTERVAL));
+    }
+}
+
+fn start(ifname: &String, config: Option<String>, traverse_nat: bool) -> anyhow::Result<()> {
+
+    wg_interface::show_config(ifname)?;
+
+    if let Some(config_file) = config {
+        let config = config::read_from_file(&config_file)?;
+        wg_interface::configure(ifname, &config)?;
+        wg_interface::show_config(ifname)?;
+        /*
+        println!("waiting for peers to attempt handshakes..");
+        std::thread::sleep(Duration::from_secs(shared::COMMON_PKA as u64 + 5));
+         */
+    }
+    monitor_interface(ifname, traverse_nat)?;
+    Ok(())
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let ifname = &cli.interface_name;
+    let traverse_nat = !cli.no_nat;
+    if let Err(e) = start(ifname, cli.config, traverse_nat) {
+        eprintln!("fatal: {e}");
+        std::process::exit(1);
     }
 }
