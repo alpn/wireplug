4421bf4a058ecfc699ee7ae1c7665ab7c4104b5e
alpn <alpn@users.noreply.github.com>
2025-08-12 21:30:20 -0400

    client/daemon: reorganize main loop; separate connectivity monitoring from peer-inactivity handling

--- client/src/daemon.rs
+++ client/src/daemon.rs
@@ -1,78 +1,115 @@
-use std::{thread, time::Duration};
+use std::{
+    thread::{self},
+    time::{Duration, Instant},
+};
 
 use anyhow::Context;
 use wireguard_control::Key;
 
 use crate::{
     announce, nat,
-    netstat::{self, NetworkMonitor},
+    netstat::{self},
     utils, wg_interface,
 };
 
 pub(crate) fn handle_inactive_peers(
     ifname: &String,
-    peers: Vec<Key>,
-    netmon: &mut NetworkMonitor,
-    listen_port: u16,
-    traverse_nat: bool,
-) -> anyhow::Result<u64> {
-    let port_to_announce = if netmon.has_changed() && traverse_nat {
-        let new_listen_port = utils::get_random_port();
-        let nat = nat::detect_kind(new_listen_port)?;
-        log::debug!("NAT: {nat:?}");
-        let observed_port = match nat {
-            nat::NatKind::Easy => new_listen_port,
-            nat::NatKind::FixedPortMapping(port_mapping_nat) => port_mapping_nat.obsereved_port,
-            nat::NatKind::Hard => {
-                log::warn!("can't do much about Hard NAT atm, will try again in a bit .. ");
-                return Ok(shared::protocol::MONITORING_INTERVAL);
-            }
-        };
-        log::debug!("updating listen port to {new_listen_port} ..");
-        std::thread::sleep(Duration::from_secs(3));
-        wg_interface::update_port(ifname, new_listen_port)?;
-        observed_port
-    } else {
-        listen_port
-    };
+    peers: &mut Vec<Key>,
+    port_to_announce: u16,
+) -> anyhow::Result<()> {
     let lan_addrs = utils::get_lan_addrs(ifname).ok();
-    match announce::announce(ifname, peers, port_to_announce, lan_addrs) {
-        Ok(response) => {
-            if wg_interface::update_peers(ifname, response)? {
-                log::info!(
-                    "some endpoints were updated, waiting for peers to attempt handshakes.."
-                );
+    for _ in 1..=3 {
+        match announce::announce(ifname, &peers, port_to_announce, &lan_addrs) {
+            Ok(response) => {
+                let peers_updated = wg_interface::update_peers(ifname, response)?;
+                if peers_updated.len() > 0 {
+                    log::info!(
+                        "some endpoints were updated, waiting for peers to attempt handshakes.."
+                    );
+                    thread::sleep(Duration::from_secs(5));
+                    peers.retain(|p| !peers_updated.contains(p));
+                }
+                return Ok(());
             }
-        }
-        Err(e) => {
-            log::warn!("announcement failed: {e}");
-            return Ok(5);
+            Err(e) => {
+                log::warn!("announcement failed: {e}");
+                thread::sleep(Duration::from_secs(5));
+            }
         }
     }
-    Ok(shared::protocol::POST_UPDATE_INTERVAL)
+    Ok(())
+}
+
+fn get_new_listen_port(traverse_nat: bool) -> Option<u16> {
+    let new_port = utils::get_random_port();
+    let new_listen_port = if traverse_nat {
+        let nat = nat::detect_kind(new_port).ok()?;
+        log::debug!("NAT: {nat:?}");
+        let observed_port = match nat {
+            nat::NatKind::Easy => Some(new_port),
+            nat::NatKind::FixedPortMapping(port_mapping_nat) => {
+                Some(port_mapping_nat.obsereved_port)
+            }
+            nat::NatKind::Hard => {
+                log::warn!("can't do much about Hard NAT atm, will try again in a bit .. ");
+                None
+            }
+        };
+        observed_port
+    } else {
+        Some(new_port)
+    };
+    return new_listen_port;
 }
 
 pub(crate) fn monitor_interface(ifname: &String, traverse_nat: bool) -> anyhow::Result<()> {
-    let mut peers_activity = wg_interface::PeersActivity::new();
     let mut netmon = netstat::NetworkMonitor::new();
+    let mut peers_activity = wg_interface::PeersActivity::new();
     wg_interface::init_peers_activity(ifname, &mut peers_activity)?;
+
     log::info!("monitoring interface: {ifname} with NAT travesal={traverse_nat}");
+
+    let peer_is_inactive_duration = Duration::from_secs(25);
+    let mut next_inactivity_check = Instant::now() + peer_is_inactive_duration;
+    let mut inactive_peers = vec![];
     loop {
-        let listen_port = wg_interface::get_port(ifname).context("listen port is not set")?;
-        let inactive_peers = wg_interface::get_inactive_peers(ifname, &mut peers_activity)?;
-        let seconds_to_wait = match inactive_peers.is_empty() {
-            true => shared::protocol::MONITORING_INTERVAL,
-            false => {
-                log::info!("{ifname} has {} INACTIVE peers", inactive_peers.len());
-                handle_inactive_peers(
-                    ifname,
-                    inactive_peers,
-                    &mut netmon,
-                    listen_port,
-                    traverse_nat,
-                )?
+        match netmon.status() {
+            netstat::NetStatus::NoChange | netstat::NetStatus::ChangedToPrev => (),
+            netstat::NetStatus::Offline => {
+                thread::sleep(Duration::from_secs(5));
+                continue;
             }
-        };
-        thread::sleep(Duration::from_secs(seconds_to_wait));
+            netstat::NetStatus::ChangedToNew => {
+                let new_listen_port = match get_new_listen_port(traverse_nat) {
+                    Some(port) => port,
+                    None => continue,
+                };
+                log::debug!("updating listen port to {new_listen_port} ..");
+                // wait before reusing the port
+                std::thread::sleep(Duration::from_secs(3));
+                wg_interface::update_port(ifname, new_listen_port)?;
+
+                inactive_peers = wg_interface::get_all_peers(ifname)?;
+                next_inactivity_check += peer_is_inactive_duration;
+            }
+        }
+        if Instant::now() > next_inactivity_check {
+            next_inactivity_check += peer_is_inactive_duration;
+            inactive_peers.clear();
+            //inactive_peers = wg_interface::get_inactive_peers_by_handshake(ifname)?;
+            inactive_peers = wg_interface::get_inactive_peers_by_txrx(ifname, &mut peers_activity)?;
+        }
+        if inactive_peers.len() > 0 {
+            log::info!("{ifname} has {} INACTIVE peers", inactive_peers.len());
+            let port_to_announce = wg_interface::get_port(ifname).context("listen port is not set")?;
+            handle_inactive_peers(ifname, &mut inactive_peers, port_to_announce)?;
+            if inactive_peers.len() > 0 {
+                next_inactivity_check = Instant::now() + Duration::from_secs(5);
+                continue;
+            }
+        } else {
+            wg_interface::show_peers(ifname)?;
+        }
+        thread::sleep(Duration::from_secs(10));
     }
 }
